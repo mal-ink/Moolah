a41dbeba48c583bb54fe90657927024fa373ae70
Malini <malini.ayyappan09@gmail.com>
2025-05-20 19:51:04 -0400

    Initial upload of site

---
+++ hi.java
@@ -0,0 +1,10 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+
+public class test{
+   public static void main(String str[]) {
+      System.out.println("Hello World");
+   }
+
+} 
---
+++ hi.js
@@ -0,0 +1,23 @@
+//my first javascript file!
+/*hell
+o
+world*/
+var myName = "Malini"; 
+myName = "John";
+myName = "Malini";
+console.log(myName);
+if (myName == "Malini") {
+    console.log("Hello Malini");
+} else if (myName == "John") {
+    console.log("Hello John");
+}
+function whatsMyName() {
+    console.log(myName);
+}
+whatsMyName();
+
+function addition(a, b) {
+    console.log(a + b) ;
+}
+addition(5, 10);
+whatsMyName();
