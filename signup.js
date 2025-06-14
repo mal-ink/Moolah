7317f3b0d5677c80c162730a01559a9a0028ba9c
Malini <malini.ayyappan09@gmail.com>
2025-06-14 13:14:37 -0400

    started backend!!!!!

---
+++ signup.js
@@ -0,0 +1,5 @@
+const form = document.getElementById('form');
+const username_input = document.getElementById('username-input');
+const email_input = document.getElementById('email-input');
+const password_input = document.getElementById('password-input');
+const repeat_password_input = document.getElementById('repeat-password-input');
