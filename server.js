2aa2274cd041c996c4ea17658f5760a2325a5494
Malini <malini.ayyappan09@gmail.com>
2025-06-15 12:43:25 -0400

    started backend fr this time

---
+++ server.js
@@ -0,0 +1,6 @@
+require('dotenv').config();
+const express = require('express');
+const nodemailer = require('nodemailer');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const app = express();
