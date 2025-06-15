6ae941319578df38b3580f6e07e45677b0dea915
Malini <malini.ayyappan09@gmail.com>
2025-06-15 15:40:34 -0400

    yahaha backend!

--- server.js
+++ server.js
@@ -3,4 +3,47 @@ const express = require('express');
 const nodemailer = require('nodemailer');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+
 const app = express();
+const PORT = 3000;
+
+// Middleware
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// POST endpoint
+app.post('/contact', (req, res) => {
+  const { firstname, lastname, email, reason } = req.body;
+
+  const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASS,
+    },
+  });
+
+  const mailOptions = {
+    from: email,
+    to: process.env.EMAIL_USER,
+    subject: 'New Contact Form Submission',
+    text: `Name: ${firstname} ${lastname}\nEmail: ${email}\nReason: ${reason}`
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.error(error);
+      res.status(500).send('Error sending message.');
+    } else {
+      console.log('Email sent: ' + info.response);
+      res.send('Message sent!');
+    }
+  });
+});
+
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
