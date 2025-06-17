0ef7a812d7325ea371b10b34494d5c78c809c46d
Malini <malini.ayyappan09@gmail.com>
2025-06-16 22:21:19 -0400

    your message

--- login.js
+++ login.js
@@ -1,2 +1,9 @@
 require('dotenv').config();
-holdurbreathwhilewedivedeepinwontutakemyhandneverletitendddd
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const app = express();
+const PORT = 3000;
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false}));
